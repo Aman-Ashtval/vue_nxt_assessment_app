69832659ef5a50b1348ae90dfc5e6c681d315ec9
Aman Kumar <9k725304@gmail.com>
2024-07-10 11:33:54 +0530

    starting from login route

---
+++ src/components/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container>
+    <el-row>
+      <el-col>
+        <div>
+          start
+        </div>
+      </el-col>
+    </el-row>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'LoginView'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
